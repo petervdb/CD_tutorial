1cccb081e0182b31eaa50aec8a393c18fed6fb0d
Peter Vandenberghe <petervanden@gmail.com>
2020-02-17 16:49:32 -0500

    added Python script

--- my_script.py
+++ my_script.py
@@ -1,6 +1,6 @@
 import datetime
 today = datetime.date.today()
-version = "1.0.4"
+version = "1.0.5"
 creation_date = "2020-02-15"
 latest_update_date = "2020-02-17"
 logfile="/log/history.log"
@@ -8,5 +8,5 @@ f = open(logfile, "a")
 print ("This container version ", version, " created on ", creation_date, " and updated on ", latest_update_date, " was last executed on ", datetime.datetime.now())
 f.write ("This container version " + version + " created on " + creation_date + " and updated on ")
 f.write ( latest_update_date + " was last executed on " +  str(datetime.datetime.now()) + "\n")
-print ("================================================================================")
-f.write("================================================================================\n")
+print ("==============================================================================================================")
+f.write("==============================================================================================================\n")
