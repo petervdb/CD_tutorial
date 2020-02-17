ca404f7507b97266baf8a74f27e07382369480d6
Peter Vandenberghe <petervanden@gmail.com>
2020-02-17 15:02:19 -0500

    added Python script

--- my_script.py
+++ my_script.py
@@ -1,6 +1,6 @@
 import datetime
 today = datetime.date.today()
-version = "1.0.1"
+version = "1.0.2"
 creation_date = "2020-02-15"
 latest_update_date = "2020-02-17"
 print ("This container version ", version, " created on ", creation_date, " and updated on ", latest_update_date, " was last executed on ", datetime.datetime.now())
