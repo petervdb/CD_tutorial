4a2f09c40400ebc1085e6576a4d2e3f1ab9c421d
Peter Vandenberghe <petervanden@gmail.com>
2020-02-17 14:54:11 +0100

    Updated Python script

--- my_script.py
+++ my_script.py
@@ -1,5 +1,5 @@
 import datetime
 today = datetime.date.today()
-
-print ("This container was last executed on ", datetime.datetime.now())
-print ("==================================================================")
+version = "1.0.0"
+print ("This container version ", version, " was last executed on ", datetime.datetime.now())
+print ("================================================================================")
